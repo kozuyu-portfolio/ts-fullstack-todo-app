9625f35f9c029984b2c725fd06422194870fd05b
kozuyu-jp <kozuyu.jp@gmail.com>
2025-05-11 03:42:51 +0000

    feat(api): integrate Prisma and generate initial schema

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TodoService } from 'todo/todo.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService,
+    private readonly todo: TodoService
+  ) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('todos')
+  getTodos() {
+    return this.todo.findAll();
+  }
+
+  @Post('todos')
+  createTodo(@Body('title') title: string) {
+    return this.todo.create({ title });
   }
 }
--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from 'prisma/prisma.module';
+import { TodoService } from 'todo/todo.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TodoService],
 })
 export class AppModule { }
