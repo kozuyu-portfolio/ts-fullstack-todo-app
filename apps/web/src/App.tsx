4992c2e9307978a0118172759a82649f5d5cc697
kozuyu-jp <kozuyu.jp@gmail.com>
2025-05-12 03:20:28 +0000

    feat(web): install OpenAPI generator and generate base client

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,3 +1,13 @@
+import { TodosService } from '@ts-fullstack-todo/api-client'
+import { hello } from '@ts-fullstack-todo/shared'
+
+export const fetchTodos = () => TodosService.todoControllerGetTodos()
+
 export function App() {
-    return <h1 className="text-2xl font-bold">Full-stack Todo App</h1>
+    return (
+        <>
+            <h1 className="text-2xl font-bold">Full-stack Todo App </h1>
+            <h2 className="text-xl font-bold">{hello('World')}</h2>
+        </>
+    )
 }
---
+++ packages/api-client/src/index.ts
@@ -0,0 +1,18 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { SignInRequestDto } from './models/SignInRequestDto';
+export type { SignInResponseDto } from './models/SignInResponseDto';
+export type { SignUpRequestDto } from './models/SignUpRequestDto';
+export type { SignUpResponseDto } from './models/SignUpResponseDto';
+export type { TodoModel } from './models/TodoModel';
+
+export { AppService } from './services/AppService';
+export { AuthService } from './services/AuthService';
+export { TodosService } from './services/TodosService';
