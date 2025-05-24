988df9365f07a452bfad8835605a0f05f3b498d3
kozuyu-jp <kozuyu.jp@gmail.com>
2025-05-24 21:48:20 +0000

    fix(api): add Type decorator for deadline property in UpdateTaskRequestDto

--- apps/api/src/task/dto/update-task.request.dto.ts
+++ apps/api/src/task/dto/update-task.request.dto.ts
@@ -1,4 +1,5 @@
 import { ApiPropertyOptional } from '@nestjs/swagger'
+import { Type } from 'class-transformer'
 import { IsBoolean, IsDate, IsOptional, IsString } from 'class-validator'
 
 export class UpdateTaskRequestDto {
@@ -15,5 +16,6 @@ export class UpdateTaskRequestDto {
     @ApiPropertyOptional({ type: Date })
     @IsDate()
     @IsOptional()
+    @Type(() => Date)
     deadline?: Date
 }
